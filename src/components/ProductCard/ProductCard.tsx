d9cb7e76f3b0008e5b6c8f8e0f55d75819604a9a
Kontantin Alekseev <alexcseev1990@gmail.com>
2023-10-24 18:08:26 +0300

    add ProductCard component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,33 @@
+import styles from "./ProductCard.module.css";
+import cn from "classnames";
+import { ProductCardProps } from "./ProductCard.props";
+import { Link } from "react-router-dom";
+
+const ProductCard = (props: ProductCardProps) => {
+    return (
+        <Link to={"/"} className={styles['link']}>
+            <div className={styles['card']}>
+                <div className={styles['head']} style={{backgroundImage: `url('${props.image}')`}}>
+                    <div className={styles['price']}>
+                        {props.price}
+                        <span className={styles['currency']}>$</span>
+                    </div>
+                    <button className={styles['add-to-cart']}>
+                        <img src="/card-cart.svg" alt="" />
+                    </button>
+                    <div className={styles['rating']}>
+                        {props.rating}
+                        <img src="/star.svg" alt="" />
+                    </div>
+                </div>
+                <div className={styles['footer']}>
+                    <h3 className={styles['title']}>{props.title}</h3>
+                    <p className={styles['description']}>{props.description}</p>
+                </div>
+            </div>
+        </Link>
+        
+    )
+}
+
+export default ProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import { Search } from "../../components/Search/Search"
 import { Headling } from "../../components/Headling/Headling"
 import styles from "./Menu.module.css"
+import ProductCard from "../../components/ProductCard/ProductCard"
 
 export const Menu = () => {
     return (
@@ -9,7 +10,18 @@ export const Menu = () => {
                 <Headling>Hello World</Headling>
                 <Search type="text" placeholder="Введите блюдо или состав" />
             </div>
-            
+            <div>
+                <ProductCard 
+                    id={1}
+                    title="Title"
+                    description="Description"
+                    rating={4.5}
+                    price={100}
+                    image="/main.jpg"
+                />
+
+               
+            </div>
         </>
     )
 }
